890b8f1c0c5d63434ee4962c83d8b8e2e45c3c61
Almost Engineer <bitsecondal@gmail.com>
2022-01-04 15:04:15 -0600

    #1 Added Ds18B20 sensor readings

--- Almostengr.WeatherStation/Sensors/DS18B20Sensor.cs
+++ Almostengr.WeatherStation/Sensors/DS18B20Sensor.cs
@@ -12,13 +12,8 @@ namespace Almostengr.WeatherStation.Sensors
         {
             string temp = string.Empty;
 
-            // reference https://github.com/dotnet/iot/tree/main/src/devices/OneWire
-
-            // Quick and simple way to find a thermometer and print the temperature
             foreach (var dev in OneWireThermometerDevice.EnumerateDevices())
             {
-                // Console.WriteLine($"Temperature reported by '{dev.DeviceId}': " +
-                //                     (await dev.ReadTemperatureAsync()).DegreesCelsius.ToString("F2") + "\u00B0C");
                 temp = (await dev.ReadTemperatureAsync()).DegreesCelsius.ToString("F2");
             }
 
