4ff1862a802f560e6e7ae86daad4faf2584e875a
Almost Engineer <bitsecondal@gmail.com>
2022-01-04 10:49:39 -0600

    Added loging for changes to database

--- Almostengr.WeatherStation/Repository/ObservationRepository.cs
+++ Almostengr.WeatherStation/Repository/ObservationRepository.cs
@@ -2,21 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Almostengr.WeatherStation.Repository;
 using Almostengr.WeatherStation.DataTransferObjects;
 using Almostengr.WeatherStation.Models;
 using Almostengr.WeatherStation.Repository.Interface;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Almostengr.WeatherStation.Repository
 {
     public class ObservationRepository : BaseRepository, IObservationRepository
     {
         private readonly StationDbContext _dbContext;
+        private readonly ILogger<ObservationRepository> _logger;
 
-        public ObservationRepository(StationDbContext dbContext) : base(dbContext)
+        public ObservationRepository(StationDbContext dbContext, ILogger<ObservationRepository> logger) : base(dbContext)
         {
             _dbContext = dbContext;
+            _logger = logger;
         }
 
         public async Task DeleteOldObservationsAsync(int retentionDays)
@@ -25,6 +27,8 @@ namespace Almostengr.WeatherStation.Repository
             
             List<Observation> observations = await _dbContext.Observations.Where(o => o.Created < cutoffDate).ToListAsync();
             _dbContext.Observations.RemoveRange(observations);
+
+            _logger.LogInformation($"Deleted {observations.Count} observations");
         }
 
         public async Task CreateObservationAsync(ObservationDto observationDto)
@@ -38,6 +42,8 @@ namespace Almostengr.WeatherStation.Repository
             observation.Created = DateTime.Now;
 
             var newObservation = await _dbContext.Observations.AddAsync(observation);
+
+            _logger.LogInformation($"Created observation {newObservation.Entity.ObservationId}");
         }
 
         public async Task<List<ObservationDto>> GetAllObservationsAsync()
