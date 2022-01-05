6ddacbdf41dc479d3a8cfe279450dcf5eebd7d6b
Kenny Robinson <bitsecondal@gmail.com>
2022-01-05 10:18:26 -0600

    Updated create and delete methods

--- Almostengr.WeatherStation/Services/ObservationService.cs
+++ Almostengr.WeatherStation/Services/ObservationService.cs
@@ -9,23 +9,62 @@ namespace Almostengr.WeatherStation.Services
     public class ObservationService : BaseService, IObservationService
     {
         private readonly IObservationRepository _repository;
+        private readonly ILogger<ObservationService> _logger;
 
-        public ObservationService(IObservationRepository repository)
+        public ObservationService(IObservationRepository repository, ILogger<ObservationService> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
 
         public async Task DeleteOldObservationsAsync(int retentionDays)
         {
             if (retentionDays > 0)
             {
-                await _repository.DeleteOldObservationsAsync(retentionDays);
+                DateTime cutoffDate = DateTime.Now.AddDays(-retentionDays);
+                List<ObservationDto> observationDtos = await _repository.GetObservationsOlderThanDate(cutoffDate);
+
+                if (observationDtos.Count > 0)
+                {
+                    try 
+                    {
+                        await _repository.DeleteObservationsAsync(observationDtos);
+                        await _repository.SaveChangesAsync();
+
+                        _logger.LogInformation($"Deleted {observationDtos.Count} observations");
+                    }
+                    catch(Exception ex)
+                    {
+                        _logger.LogError(ex, ex.Message);
+                    }
+                }
             }
         }
 
-        public async Task CreateObservationAsync(ObservationDto observation)
+        public async Task<ObservationDto> CreateObservationAsync(ObservationDto observationDto)
         {
-            await _repository.CreateObservationAsync(observation);
+            try 
+            {
+                if (observationDto == null)
+                {
+                    throw new ArgumentNullException(nameof(observationDto));
+                }
+
+                Observation observation = new Observation(observationDto);
+                observation.Created = DateTime.Now;
+
+                var newObservation = await _dbContext.Observations.AddAsync(observation);
+                await _dbContext.SaveChangesAsync();
+
+                _logger.LogInformation($"Created observationId {newObservation.Entity.ObservationId}");
+                
+                return new ObservationDto(newObservation);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return null;
+            }
         }
 
         public async Task<IList<ObservationDto>> GetAllObservationsAsync()
