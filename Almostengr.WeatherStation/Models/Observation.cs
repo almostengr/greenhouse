39bf8be9f45a4a3e4188d9d3e04df7e1f4427da5
Kenny Robinson <bitsecondal@gmail.com>
2022-01-05 10:25:49 -0600

    Added extension method for DTO

--- Almostengr.WeatherStation/Models/Observation.cs
+++ Almostengr.WeatherStation/Models/Observation.cs
@@ -49,6 +49,18 @@ namespace Almostengr.WeatherStation.Models
                 Created = o.Created
             };
         }
+        
+        public static ObservationDto AsDto(this Observation observation)
+        {
+            return new ObservationDto{
+                ObservationId = observation.ObservationId,
+                TemperatureC = Math.Round(observation.TemperatureC, Constants.MAX_DECIMAL_PLACES),
+                TemperatureF = Math.Round(((observation.TemperatureC * 1.8) + 32), Constants.MAX_DECIMAL_PLACES),
+                Humidity = observation.Humidity,
+                Pressure = observation.Pressure,
+                Created = observation.Created
+            };
+        }
 
     }
 }
