ea50be98bbd0f2cf4670790c9d5e5f4a31189821
Almost Engineer <bitsecondal@gmail.com>
2022-01-11 22:29:11 -0600

    Updated namespace; project dependencies

--- Almostengr.GardenMgr.Common/AppSettings.cs
+++ Almostengr.GardenMgr.Common/AppSettings.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Almostengr.WeatherStation.Api
+namespace Almostengr.GardenMgr.Common
 {
     public class AppSettings
     {
