db4c2a8cc1f75117c561ab6fe6819ded54e4514e
Almost Engineer <bitsecondal@gmail.com>
2022-01-12 06:52:07 -0600

    Commented nws weather worker

--- Almostengr.GardenMgr.WeatherStation/Workers/NwsWeatherWorker.cs
+++ Almostengr.GardenMgr.WeatherStation/Workers/NwsWeatherWorker.cs
@@ -14,67 +14,67 @@ namespace Almostengr.Greenhouse.Api.Workers
 {
     public class NwsWeatherWorker : BaseWorker
     {
-        public readonly AppSettings _appSettings;
-        private readonly IPrecipitationRepository _precipitationRepository;
-        private readonly ITemperatureRepository _temperatureRepository;
-        private readonly ILogger<NwsWeatherWorker> _logger;
-        private HttpClient _httpClient;
+        // public readonly AppSettings _appSettings;
+        // private readonly IPrecipitationRepository _precipitationRepository;
+        // private readonly ITemperatureRepository _temperatureRepository;
+        // private readonly ILogger<NwsWeatherWorker> _logger;
+        // private HttpClient _httpClient;
 
-        public NwsWeatherWorker(ILogger<NwsWeatherWorker> logger, ITwitterClient twitterClient, AppSettings appSettings,
-            IPrecipitationRepository precipitationRepository, ITemperatureRepository temperatureRepository) :
-            base(logger, twitterClient, appSettings)
-        {
-            _appSettings = appSettings;
-            _precipitationRepository = precipitationRepository;
-            _temperatureRepository = temperatureRepository;
-            _logger = logger;
+        // public NwsWeatherWorker(ILogger<NwsWeatherWorker> logger, ITwitterClient twitterClient, AppSettings appSettings,
+        //     IPrecipitationRepository precipitationRepository, ITemperatureRepository temperatureRepository) :
+        //     base(logger, twitterClient, appSettings)
+        // {
+        //     _appSettings = appSettings;
+        //     _precipitationRepository = precipitationRepository;
+        //     _temperatureRepository = temperatureRepository;
+        //     _logger = logger;
 
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri("");
-        }
+        //     _httpClient = new HttpClient();
+        //     _httpClient.BaseAddress = new Uri("");
+        // }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            List<string> weatherStationIds = new();
-            weatherStationIds.Add("https://api.weather.gov/stations/KMGM/observations/latest");
+        // protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        // {
+        //     List<string> weatherStationIds = new();
+        //     weatherStationIds.Add("https://api.weather.gov/stations/KMGM/observations/latest");
 
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                foreach (var weatherStationUrl in weatherStationIds)
-                {
-                    var observation = await HttpGetAsync<NwsObservationLatestDto>(_httpClient, weatherStationUrl);
+        //     while (!stoppingToken.IsCancellationRequested)
+        //     {
+        //         foreach (var weatherStationUrl in weatherStationIds)
+        //         {
+        //             var observation = await HttpGetAsync<NwsObservationLatestDto>(_httpClient, weatherStationUrl);
 
-                    if (observation == null)
-                    {
-                        _logger.LogError($"Failed to get weather data from {weatherStationUrl}");
-                        continue;
-                    }
+        //             if (observation == null)
+        //             {
+        //                 _logger.LogError($"Failed to get weather data from {weatherStationUrl}");
+        //                 continue;
+        //             }
 
-                    try
-                    {
-                        var temperature = new Temperature(observation);
-                        await _temperatureRepository.AddAsync(temperature);
-                        await _temperatureRepository.SaveAsync();
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex, ex.Message);
-                    }
+        //             try
+        //             {
+        //                 var temperature = new Temperature(observation);
+        //                 await _temperatureRepository.AddAsync(temperature);
+        //                 await _temperatureRepository.SaveAsync();
+        //             }
+        //             catch (Exception ex)
+        //             {
+        //                 _logger.LogError(ex, ex.Message);
+        //             }
 
-                    try
-                    {
-                        var precipitation = new Precipitation(observation);
-                        await _precipitationRepository.AddAsync(precipitation);
-                        await _precipitationRepository.SaveAsync();
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex, ex.Message);
-                    }
-                }
+        //             try
+        //             {
+        //                 var precipitation = new Precipitation(observation);
+        //                 await _precipitationRepository.AddAsync(precipitation);
+        //                 await _precipitationRepository.SaveAsync();
+        //             }
+        //             catch (Exception ex)
+        //             {
+        //                 _logger.LogError(ex, ex.Message);
+        //             }
+        //         }
 
-                await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
-            }
-        }
+        //         await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
+        //     }
+        // }
     }
 }
